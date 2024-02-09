4506f623dd8da12134564cc4fd795870f58943ea
Abhi Stephen Rokkam <117331604+Abhi-Stephen@users.noreply.github.com>
2024-02-09 15:29:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 # main.py
+"""
+This module contains functions for greeting and addition.
+"""
+
 def hello_world():
+    """Prints a simple greeting."""
     print("Hello, World!")
 
 if __name__ == "__main__":
