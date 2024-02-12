699b1646cdf071d3f780699abd95645a9a707f53
vineeth ketham <vineethketham@gmail.com>
2024-02-11 23:23:42 -0600

    unit testing

--- src/__tests__/unittest.test.jsx
+++ src/__tests__/unittest.test.jsx
@@ -1,15 +1,59 @@
+// _tests_/unittests.test.jsx
 import React from 'react';
-import { render, screen, fireEvent, cleanup } from '@testing-library/react';
-import Login from '../Components/Login.js';
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Login from '../Components/Login';
 
-test('input values change correctly', () => {
-    const { getByLabelText } = render(<Login />);
+describe('Login Component', () => {
+  test('renders login form correctly', () => {
+    const { getByLabelText, getByText } = render(<Login />);
+    
     const emailInput = getByLabelText('Email address');
     const passwordInput = getByLabelText('Password');
+    const submitButton = getByText('Submit');
+
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  test('updates input value', () => {
+    // Render the Login component
+    const { getByLabelText } = render(<Login />);
+    
+    // Get input elements by their respective labels
+    const emailInput = getByLabelText('Email address');
+    const passwordInput = getByLabelText('Password');
+  
+    // Simulate user input by changing the input values
+    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'testpassword' } });
+  
+    // Check if the input values are updated correctly
+    expect(emailInput.value).toBe('test@example.com');
+    expect(passwordInput.value).toBe('testpassword');
+  });
+
+  
+
+  test('handles form submission correctly with valid credentials', async () => {
+    const fakeAuthToken = 'fakeAuthToken';
+    jest.spyOn(window, 'fetch').mockResolvedValueOnce({
+      json: () => Promise.resolve({ success: true, authtoken: fakeAuthToken }),
+    });
+
+    const { getByLabelText, getByText } = render(<Login />);
+
+    const emailInput = getByLabelText('Email address');
+    const passwordInput = getByLabelText('Password');
+    const submitButton = getByText('Submit');
 
     fireEvent.change(emailInput, { target: { value: 'email@gmail.com' } });
     fireEvent.change(passwordInput, { target: { value: 'password' } });
+    fireEvent.click(submitButton);
 
-    expect(emailInput.value).toBe('email@gmail.com');
-    expect(passwordInput.value).toBe('password');
-});
+    await waitFor(() => expect(localStorage.getItem('token')).toEqual(fakeAuthToken));
+    expect(window.location.href).toBe('http://localhost/');
+  });
+
+  
