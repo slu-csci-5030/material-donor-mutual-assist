32e67fe40bc14b557e11341f1fe2dee5d86e0259
Raghunath Kunigiri <54011917+Raghunath9493@users.noreply.github.com>
2024-02-20 12:47:15 -0600

    Update crud_donations.js
    Added test cases

--- backend/donor-module/routes/crud_donations.js
+++ backend/donor-module/routes/crud_donations.js
@@ -3,6 +3,9 @@ var router = express.Router();
 const donations = [
   { id: 1, name: 'Alice', donations: 'Laptop', Quantity: '1'},
   { id: 2, name: 'Robby', donations: 'Microphone', Quantity: '1'},
+  { id: 3, name: 'Vicky', donations: 'Mobile', Quantity: '1'},
+  { id: 4, name: 'Ramu', donations: 'Bicycle', Quantity: '1'},
+  { id: 5, name: 'John', donations: 'Computer', Quantity: '1'},
 ]; //will be replaced with real database connection
 /* GET donations listing. */
 router.get('/', function(req, res, next) {
