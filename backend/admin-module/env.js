55032300fed364d1de4f7ddf5e2d580952e1f67b
Ajay Kumar Medikonda <ajaykumar.medikonda@slu.edu>
2024-02-25 11:44:53 -0600

    added extra module

--- backend/admin-module/env.js
+++ backend/admin-module/env.js
@@ -2,3 +2,7 @@ module.exports = {
     EMAIL : 'manoharmeda6@gmail.com',
     PASSWORD : 'qzrz ywwq vtnw wdzm'
 }
+module.exports = {
+    EMAIL : 'ajaykumarmedikonda1@gmail.com',
+    PASSWORD : 'ajsy jnd kyv'
+}
