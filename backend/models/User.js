254d7f5692379066578e937e8f4b00db267bb354
MarySreejaReddy <sreejureddytr292@gmail.com>
2024-02-25 11:57:21 -0600

    Password requirements

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,24 +1,28 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
 const UserSchema = new Schema({
-    name:{
+    name: {
         type: String,
         required: true
     },
-    email:{
+    email: {
         type: String,
         required: true,
         unique: true
     },
-    password:{
+    password: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: function (password) {
+                // Define your password requirements using a regular expression
+                // For example, requiring at least one uppercase letter, one lowercase letter, and one digit
+                const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+                return passwordRegex.test(password);
+            },
+            message: 'Password must contain at least one uppercase letter, one lowercase letter, and one digit, and be at least 8 characters long.'
+        }
     },
-    date:{
+    date: {
         type: Date,
         default: Date.now
     },
-  });
-  const User = mongoose.model('user', UserSchema);
-  module.exports = User;    
+});
