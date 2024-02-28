8c5fb72378006cfea78b0cfcc2d142b4c8798e15
Deepak <deepak.pvsn@gmail.com>
2024-02-28 03:32:22 -0600

    Admin's Donated Items List Page

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import RegisterPage from './components/RegisterPage';
+import AdminHeader from './components/AdminHeader';
+import DonatedItemsList from './components/DonatedItemsList';
 
 const App = () => {
   return (
@@ -9,6 +11,10 @@ const App = () => {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/donations" element={<>
+          <AdminHeader />
+          <DonatedItemsList />
+        </>} />
       </Routes>
     </Router>
   );
