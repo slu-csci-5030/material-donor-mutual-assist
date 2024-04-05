5bd3630ccf8877e2b97468fbf4460c87483ab02d
Sravya-Padala <padalasravya1508@gmail.com>
2024-04-05 08:47:11 -0500

    Add overview for inventory

--- backend/admin-module/routes/admin-details/crud_inventory.js
+++ backend/admin-module/routes/admin-details/crud_inventory.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var router = express.Router()
 const inventory = require('../../database/inventory-model')
+const Inventory = require('../../database/inventory-model')
 
 router.get('/:adminId', async (req, res) => {
     try {
@@ -15,3 +16,38 @@ router.get('/:adminId', async (req, res) => {
         res.status(500).json({message: 'Error fetching inventory added by admin'})
     }
 })
+
+router.get('/getAllInventory', async (req, res) => {
+    try {
+        var inventoryList = await inventory.findAll()
+        res.json(inventoryList).status(200)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: "Error fetching all inventory"})
+    }
+})
+
+router.get('/overview', async (req, res) => {
+    try{
+        const newInventory = await Inventory.findAll({
+            group: ['categoryId']
+        });
+        res.json({ message: "Inventory Item added successfully"}).status(200)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: "Error adding inventory"})
+    }
+})
+
+router.post('/', async (req, res) => {
+    try{
+        const newInventory = await Inventory.create(req.body);
+        res.json({ message: "Inventory Item added successfully"}).status(200)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: "Error adding inventory"})
+    }
+})
+
+
+module.exports = router
