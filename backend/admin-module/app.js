eb3ece273e855d069280fa7597003b8a8a7291b4
Raghunath Kunigiri <54011917+Raghunath9493@users.noreply.github.com>
2024-05-05 10:42:19 -0500

    Update app.js

--- backend/admin-module/app.js
+++ backend/admin-module/app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
-
 var indexRouter = require('./routes/index');
+
 var usersRouter = require('./routes/users');
 var donarListRouter = require('./routes/donor-details/donars_List');
 var donar_module_router = require('./routes/demo/donar-module');
